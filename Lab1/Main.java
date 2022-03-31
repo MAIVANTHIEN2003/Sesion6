5f0b2597e94a51bb4adb5343e5847cfbf5c4c1b4
MAIVANTHIEN2003 <maithien2003ns@gmail.com>
2022-03-31 21:49:45 +0700

    BT

---
+++ Lab1/Main.java
@@ -0,0 +1,10 @@
+package Session6.Lab1;
+
+import Session2.Sinhvien;
+
+public class Main {
+    public static void main(String [] agrs){
+        SinhVien sv = new SinhVien();
+        sv.TT();
+    }
+}
